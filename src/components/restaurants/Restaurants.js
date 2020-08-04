d1f4961843caa0a2f2e86f451a121b16fe4704d7
antdp425 <antdp425@aol.com>
2020-08-04 08:06:33 -0400

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import Restaurant from "./Restaurant"
 
 class Restaurants extends Component {
   render() {
+
+    const rList = this.props.restaurants.map(r => {
+                    return <Restaurant key={r.id} 
+                                restaurant={r} 
+                                addReview={this.props.addReview}
+                                deleteRestaurant={this.props.deleteRestaurant} 
+                                reviews={this.props.reviews.filter(review => review.restaurantId === r.id )}/>
+                    })
+    
     return(
       <ul>
-        Restaurants Component
+        {rList}
       </ul>
     );
   }
